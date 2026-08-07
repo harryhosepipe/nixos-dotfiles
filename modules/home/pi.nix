156673d1fa24e59f0a4b7a44d92a935061c3b02f
Pablo <pablo@renderbros.com>
2026-08-07 18:05:37 +0200

    fix(pi): package coding agent declaratively

--- modules/home/pi.nix
+++ modules/home/pi.nix
@@ -1,11 +1,37 @@
 { pkgs, ... }:
 let
-  piVersion = "0.82.1";
+  piVersion = "0.83.0";
+
+  piPackage = pkgs.buildNpmPackage {
+    pname = "pi-coding-agent";
+    version = piVersion;
+
+    src = ../../nix/pi-npm;
+    npmDepsHash = "sha256-akHVCJv815QCoo580q44irk3XqpQ8oxtxHmWVof/msg=";
+    npmDepsFetcherVersion = 2;
+
+    dontNpmBuild = true;
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p "$out/lib/pi-coding-agent" "$out/bin"
+      cp -r node_modules package.json package-lock.json "$out/lib/pi-coding-agent/"
+
+      makeWrapper ${pkgs.nodejs_24}/bin/node "$out/bin/pi" \
+        --add-flags "$out/lib/pi-coding-agent/node_modules/@earendil-works/pi-coding-agent/dist/cli.js" \
+        --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.ripgrep pkgs.fd ]}
+
+      runHook postInstall
+    '';
+
+    meta.mainProgram = "pi";
+  };
 
   pi = pkgs.writeShellScriptBin "pi" ''
-    export PATH=${pkgs.nodejs_22}/bin:$PATH
     unset PI_CODING_AGENT_DIR PI_CODING_AGENT_SESSION_DIR PI_PACKAGE_DIR
-    exec ${pkgs.nodejs_22}/bin/npx --prefer-offline -y @earendil-works/pi-coding-agent@${piVersion} "$@"
+    exec ${piPackage}/bin/pi "$@"
   '';
 in
 {
